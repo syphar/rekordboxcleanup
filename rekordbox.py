afe28c3a87c1955cb6a26b16b4eba21e40a7f304
Denis Cornehl <denis.cornehl@gmail.com>
2019-10-18 18:01:16 +0200

    cleanup

--- rekordbox.py
+++ rekordbox.py
@@ -12,12 +12,6 @@ from commands.trackswithoutplaylist import trackswithoutplaylist
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 
-# TODO: action - list playlists with id
-# TODO: action - tracks shared in defined playlists (only manual lists)
-# TODO: action - list of tracks in more than 1 pl (only manual lists)
-# TODO: action - remove tracks from one PL that are in any other manual
-
-
 @click.group()
 def cli():
     pass
@@ -27,20 +21,6 @@ cli.add_command(findduplicatetracks)
 cli.add_command(trackswithoutplaylist)
 cli.add_command(additionalfiles)
 
-
-@click.command()
-@click.argument('folder')
-def test(folder):
-    f = Path(folder)
-    for item in f.iterdir():
-        pprint(item)
-        pprint(str(item))
-        pprint(list(str(item)))
-        pprint(list(bytes(item).decode('utf-8')))
-        pprint(list(bytes(item)))
-
-
-cli.add_command(test)
 
 if __name__ == '__main__':
     cli()
